bf9dc3d0f9ab9650d1d177a2f7b0b21716e44774
Abdellatif-ait <ja_aiteur@esi.dz>
2023-01-08 15:06:24 +0100

    add Déconnexion features

--- src/main/java/com/telly/controllers/userController.java
+++ src/main/java/com/telly/controllers/userController.java
@@ -1,7 +1,14 @@
 package com.telly.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class userController {
+    @RequestMapping("/login")
     public String showLogin() {
         return "login";
+    }
+    @RequestMapping("/loggedout")
+    public String showLogout() {
+        return "loggedout";
     }
 }
